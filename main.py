bf9228e4b11b91b4a3fb7284f02fb10fb2619c22
Nosakhare Belvi <nosakharebelvi@gmail.com>
2017-08-30 22:32:13 +0100

    handling content pull on client

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ from github import Github
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-
-
 import webapp2
 
 urlfetch.set_default_fetch_deadline(60)
@@ -36,15 +34,18 @@ jinja_environment = jinja2.Environment(autoescape=True, loader=jinja2.FileSystem
 
 class MainHandler(webapp2.RequestHandler):
     def get(self, *ar, **kw):
+        # split the path with '/' to get detail
         path = str(ar[0]).split('/')
-        gitUserName = path[0]
-        repoName = path[1]
+        author = str(path[0])
+        repoName = str(path[1])
         labPath = ""
         for index in range(2, len(path)):
             if len(str(path[index]).strip()) != 0:
                 labPath += path[index] + "/"
-        g = Github(client_id="74a6ead0dcdde0710c40", client_secret="14bbd96eb781620284b5794e657e04bbd4ebf3ff")
-        user = g.get_user(gitUserName)
+
+        # init git credentials
+        g = Github(client_id="", client_secret="")
+        user = g.get_user(author)
         repo = user.get_repo(repoName)
         variables = {}
         data = list()
@@ -57,19 +58,19 @@ class MainHandler(webapp2.RequestHandler):
         steps = list()
         for step in manifest['steps']:
             steps.append(step)
-        for step in steps:
             dataEntry = {}
-            file = repo.get_file_contents(labPath + str(step) + ".md")
-            decoded_content = file.decoded_content
             dataEntry['label'] = manifest[str(step)]['label']
-            dataEntry['data'] = str(decoded_content).decode("utf8")
             dataEntry['id'] = str(step)
+            dataEntry['data_link'] = buildRawFileContentPath(author, repoName, labPath, str(step))
             data.append(dataEntry)
 
         variables['data'] = data
-
         template = jinja_environment.get_template('codelab.html')
         self.response.write(template.render(variables))
+
+
+def buildRawFileContentPath(author, repo, path, filename):
+    return "https://raw.githubusercontent.com/" + author + "/" + repo + "/master/" + path + filename + ".md"
 
 
 run_wsgi_app(webapp.WSGIApplication(
